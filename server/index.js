3c906fce512a763ddde028f2eb100bfdf1674230
Sunnykrp <sunnykrp3@gmail.com>
2025-07-25 16:53:53 +0530

    feat:fixed backend

--- server/index.js
+++ server/index.js
@@ -1,30 +1,38 @@
-const express=require('express');
-const mongoose=require('mongoose');
-const dotenv=require('dotenv');
-const cookieParser=require('cookie-parser');
-const path=require('path');
-const authRoutes=require('./routes/authRoutes.js');
-const cors = require('cors');
-const postRoutes=require('./routes/postRoutes.js');
+const express = require("express");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const cookieParser = require("cookie-parser");
+const path = require("path");
+const authRoutes = require("./routes/authRoutes.js");
+const cors = require("cors");
+const postRoutes = require("./routes/postRoutes.js");
 
 dotenv.config();
-const app=express();
+const app = express();
 
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors({
-    origin: 'http://localhost:5174', // Adjust this to your client URL
+app.use(
+  cors({
+      origin: process.env.CLIENT_ORIGIN || "http://localhost:5174",
     credentials: true, // Allow cookies to be sent with requests
-}));
-app.use('/api/auth', authRoutes);
-app.use('/api/posts',postRoutes);
-const PORT=process.env.PORT||3000;
-const MONGO_URI=process.env.MONGO_URI;
+  })
+);
+app.use("/api/auth", authRoutes);
+app.use("/api/posts", postRoutes);
+const PORT = process.env.PORT || 3000;
+const MONGO_URI = process.env.MONGO_URI;
 
-mongoose.connect(MONGO_URI,{useNewUrlParser:true,useUnifiedTopology:true})
-.then(()=>{
-    console.log('Connected to MongoDB');
-    app.listen(PORT,()=>{
-        console.log(`Server is running on port ${PORT}`);
-    })
-});
+mongoose
+  .connect(MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    ssl: true,
+    tlsInsecure: false,
+  })
+  .then(() => {
+    console.log("Connected to MongoDB");
+    app.listen(PORT, () => {
+      console.log(`Server is running on port ${PORT}`);
+    });
+  });
